17a1888f69eb113b31790d8a190d8f5d1fa3eacb
James Youn <jamesyoun710@gmail.com>
2015-06-27 13:02:02 -0700

    fix user index error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
-	before_action :set_user, only: [:index, :show]
+	before_action only: [:index, :show]
 
 	def index
 		@users = User.all
 	end
 
 	def show
-		@user
+		@user = User.find(params[:id])
 	end
 
 	private
 
-		def set_user
-			@user = User.find(params[:id])
-		end
+		# def set_user
+		# 	@user = User.find(params[:id])
+		# end
 		
 		def user_params
 			params.require(:user).permit(memberships_attributes: [:group_name])

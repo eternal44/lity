c6647d86cdea9a966231253d14dad96df68b7e9d
James Youn <jamesyoun710@gmail.com>
2015-08-16 10:35:47 -0700

    Add model/group folder

---
+++ app/models/group/archive.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: archives
+#
+#  id          :integer          not null, primary key
+#  group_id    :integer
+#  user_id     :integer
+#  lift_name   :string
+#  lift_weight :integer
+#  video_link  :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Group::Archive < ActiveRecord::Base
+	belongs_to :group
+	belongs_to :user
+
+  default_scope -> { order(created_at: :desc) }
+
+  validates :user_id, presence: true
+  validates :group_id, presence: true
+  validates :lift_name, presence: true
+  validates :lift_weight, presence: true
+  validates_format_of :video_link, :with => /\A(https\:\/\/)?((www\.)?youtube\.com|youtu\.?be)\/.+$\Z/, :allow_blank => true, :message => "must be a valid YouTube URL"
+
+end

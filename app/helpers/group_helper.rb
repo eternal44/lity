510b228413ae56f7747ba27fcbf6fb19b6e803b6
James Youn <jamesyoun710@gmail.com>
2015-08-18 13:11:50 -0700

    Add regular member authorization for archive form

--- app/helpers/group_helper.rb
+++ app/helpers/group_helper.rb
@@ -2,4 +2,8 @@ module GroupHelper
 	def admin(group)
 		true if group.memberships.admin.where(user_id: current_user.id).present?
 	end
+
+	def regular(group)
+		true if group.memberships.member.where(user_id: current_user.id).present?
+	end
 end
